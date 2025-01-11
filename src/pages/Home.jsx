765543e158c3c0e506cae1fc74c14ac97b590f17
Abhishek Sharma <abhishekjps19@gmail.com>
2025-01-11 09:24:35 +0530

    Home and Contact Us page created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,126 +1,120 @@
 import React from "react";
+import "../styles/Home.css";
+import { FaGamepad, FaDiscord } from "react-icons/fa";
+import gameImage from "../games/LaserBlast/assets/thumbnail.webp";
+import { Link } from "react-router-dom";
+import gameConfigTemporary from "../routes/gameConfigTemporary";
 
-function Home(props) {
+const HomePage = () => {
+  const games = [
+    {
+      title: "Laser Blast",
+      description: "Invaders must die!",
+      type: "Single Player",
+      path: "/laser-blast",
+      //imgSrc: laserBlastLogo,
+      //element: <LaserBlast />,
+    },
+    {
+      title: "YOLO",
+      description:
+        "Deposit crypto and spin the wheel. But remember: there can be only one winner.",
+      image: gameImage,
+      watching: 31,
+    },
+    {
+      title: "Moon or Doom",
+      description:
+        "Think you know where crypto prices are headed in the next 60 seconds? Put your money where your...",
+      image: gameImage,
+      watching: 7,
+      //link: <LaserBlast />,
+    },
+  ];
+
   return (
-    <>
-      <h1>This is a home page</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque cum
-        architecto quas debitis. Nobis deserunt possimus nulla quibusdam quae
-        ipsa, repudiandae, quis non earum, minima nostrum expedita tempore culpa
-        accusantium debitis molestiae. Accusantium libero rerum dolorum
-        voluptatem? Rerum tempora, blanditiis totam quo, alias quam non ut eum
-        quod repudiandae autem fuga maiores nihil optio enim accusantium iusto.
-        Nesciunt aspernatur laborum illum! Quas, laborum doloremque sunt
-        voluptatem minima accusamus delectus recusandae expedita consequuntur
-        laboriosam ipsa reprehenderit a eos harum excepturi eveniet! Vitae neque
-        expedita optio veniam quo, voluptas dicta aperiam, nam, eos sed aliquam
-        tempora? Fugiat nostrum voluptates rerum, quos minus pariatur mollitia
-        reiciendis ducimus laudantium inventore perspiciatis, quibusdam tempora,
-        quis in beatae quod animi voluptatibus quae nesciunt? Incidunt velit
-        adipisci, provident voluptas, voluptatem laboriosam obcaecati quod minus
-        facilis odio fugit iste consequuntur iure architecto vitae, deleniti
-        perferendis. Dignissimos, rerum praesentium! Aperiam mollitia explicabo
-        nostrum doloribus, corrupti provident et labore magnam sit optio. Quo,
-        nemo deleniti nisi corrupti architecto tenetur quas natus rem quam
-        adipisci mollitia nihil laudantium doloremque debitis voluptate aut
-        error dolor necessitatibus quasi quibusdam alias inventore eveniet ad?
-        Qui nulla laudantium at fugiat nostrum, id, harum minus maxime ex et vel
-        recusandae expedita corporis odio modi non perferendis facilis enim
-        accusamus, esse magni nam voluptate debitis. Impedit officia iure quod,
-        repudiandae molestiae ducimus corporis enim obcaecati unde nisi facilis
-        laborum dolor? Ex, obcaecati iste quae dolorum earum quas nam sapiente
-        rerum temporibus hic aliquid doloribus! Id explicabo unde cum tenetur
-        optio reiciendis perferendis odio nihil culpa fugit dignissimos
-        reprehenderit quis saepe assumenda natus architecto corrupti ipsum,
-        maiores ea quidem numquam distinctio? Omnis quod dolorum nihil. Vel,
-        itaque beatae. Nesciunt obcaecati vero similique assumenda animi
-        voluptate quae sequi voluptatem voluptatibus maiores eos cumque
-        accusamus veritatis, illum deserunt incidunt dolores dolor et corrupti
-        ex? Vero, eaque qui! Quae neque molestiae distinctio voluptas recusandae
-        saepe fuga, est veritatis laboriosam id architecto consectetur dolorem
-        pariatur quas quasi et minima rem voluptate corporis officiis
-        repudiandae iure? Obcaecati dicta inventore eum quisquam, et accusantium
-        vero impedit minus adipisci molestias enim maxime. Quod maxime ut
-        dolorum voluptatem voluptatibus quaerat nisi possimus rerum! Nostrum
-        itaque animi hic eveniet natus aliquid illo eligendi esse, eum error
-        ipsam at, voluptas autem culpa similique deleniti, vitae quo quam
-        quaerat quod consequuntur! Magni, praesentium ratione inventore
-        voluptatibus voluptatem cupiditate velit eos odit repellendus dicta?
-        Doloremque magnam temporibus fugiat totam amet, vero optio ullam iste
-        porro necessitatibus aut eligendi veniam nostrum labore enim dolorem
-        nobis aperiam facilis velit reprehenderit quam harum mollitia excepturi
-        eius. Aliquid eum aspernatur, minus distinctio officiis facilis ducimus
-        ipsum sint in odio quos, obcaecati facere rem et tempora mollitia
-        similique libero possimus illum ullam rerum ipsa, eius expedita.
-        Cupiditate accusantium eos qui necessitatibus cum perspiciatis eligendi
-        odio quos aliquam at rem laboriosam earum reprehenderit obcaecati
-        maiores, ipsum distinctio quaerat aliquid dolorum autem in? Enim amet
-        harum numquam magnam quas explicabo blanditiis perferendis vero quos
-        quidem distinctio provident aperiam quod officiis adipisci, deleniti,
-        obcaecati debitis dolorum dignissimos suscipit quo eos culpa maiores
-        optio. Deserunt dicta architecto molestias vel odio, quisquam
-        dignissimos atque unde id sint harum fugit cupiditate quasi numquam ad
-        voluptas, reprehenderit commodi necessitatibus nemo quam asperiores!
-        Omnis, beatae? Laudantium possimus ab dolor vero alias hic, eligendi
-        cum. Beatae quo soluta enim non in recusandae explicabo nostrum, dolores
-        nihil doloremque dolorem consequuntur impedit eveniet sapiente, placeat
-        expedita est nam suscipit modi provident excepturi veritatis aut
-        delectus! Magnam saepe explicabo exercitationem accusantium sunt magni
-        soluta, natus repellendus dolorem esse, iusto placeat architecto nam
-        molestias! Velit magni dignissimos recusandae, repellendus minus dicta
-        debitis ipsa dolore repellat perspiciatis nisi, qui nemo doloremque quas
-        tempore iusto accusamus ipsam, illo vitae saepe praesentium autem! Et
-        ullam quo vero quis unde consequuntur consequatur nesciunt repellendus,
-        optio soluta esse, tempore omnis sed iure, dolorem praesentium modi
-        laudantium quas alias eligendi. Deserunt necessitatibus hic quisquam
-        aliquid, veritatis neque molestiae accusantium praesentium provident sit
-        officia aspernatur autem a corporis! Quia numquam, eveniet est aliquid
-        quis debitis officiis vitae, odio perspiciatis delectus esse voluptas.
-        Quas, assumenda. Ipsam totam sed saepe laudantium blanditiis earum,
-        nobis animi sapiente doloribus vitae necessitatibus consequatur minima
-        delectus, officiis temporibus libero, quas sit iste maxime cumque eos
-        magni? Similique laboriosam illum in deserunt iste ad repudiandae
-        assumenda voluptatum magni reiciendis accusamus voluptatibus eaque, esse
-        obcaecati error cum quisquam nemo facere quidem adipisci labore ea
-        necessitatibus quaerat? Eius, corporis quisquam laborum fugiat
-        reiciendis sunt odit consectetur molestias est accusantium at atque
-        porro ea officiis in nostrum quia! Architecto sed non nemo quam
-        voluptatem aliquam modi! Velit non qui autem nostrum, dolor amet, nobis
-        magni voluptatem laudantium veniam ea explicabo culpa enim nemo.
-        Repellendus minima mollitia, accusantium aliquam pariatur perspiciatis
-        recusandae, fuga qui nostrum ex optio, odit sit laudantium distinctio
-        vel tempora adipisci aperiam? Rerum nesciunt non sit in, dicta itaque,
-        numquam a tempora quo nisi dolorum officiis nostrum. Libero eveniet sit
-        recusandae. Laborum dolore unde sequi magni? Magnam magni perspiciatis
-        neque doloremque sed eos voluptates debitis ipsa, libero commodi
-        accusamus eligendi perferendis iusto placeat, aliquid recusandae
-        praesentium dolores omnis similique. Repellat sit voluptate beatae,
-        aliquam facere qui! Suscipit, quae consectetur. Nobis, vitae iusto
-        aliquid obcaecati quam quisquam expedita adipisci dolor dignissimos?
-        Voluptatem doloremque rem, animi consequuntur quas distinctio harum
-        deserunt magnam possimus vitae! Iure, impedit ullam? Dolores hic ad ex
-        iure amet mollitia enim, quaerat tempore tenetur aliquid ratione nemo
-        maiores veritatis obcaecati facilis magnam reprehenderit vitae fugit
-        quia incidunt blanditiis modi labore quam perferendis. Molestiae
-        consequatur, adipisci reprehenderit natus, beatae blanditiis mollitia
-        magnam minus velit tempora quidem fugit placeat laudantium non eius
-        dolorum quis iste asperiores ullam odit necessitatibus? Alias totam
-        consectetur fugiat quasi? Maiores, veritatis? Necessitatibus molestiae
-        harum, perferendis earum ab, quos eveniet dignissimos architecto sint,
-        animi inventore veritatis? Asperiores, totam voluptatibus debitis
-        sapiente nemo nesciunt? Dolorem ea impedit repellat placeat, enim nisi
-        inventore esse quia vero eaque similique reprehenderit, hic ratione
-        delectus deserunt voluptatibus laboriosam quasi quos dignissimos iusto
-        temporibus, neque pariatur eius. Minima vero obcaecati, aut neque
-        quaerat unde reiciendis dolorum esse perferendis odit error dolore in
-        debitis accusamus itaque rem laudantium minus magni, doloremque
-        voluptatum. Voluptates inventore recusandae corporis.
-      </p>
-    </>
+    <div className="homepage">
+      {/* Header */}
+      <header className="homepage-header">
+        <div className="logo">
+          <span className="highlight">YOLO</span> GAMES
+        </div>
+      </header>
+
+      {/* Popular Games Section */}
+      <section className="popular-games">
+        <h2>
+          <FaGamepad size={18} /> POPULAR GAMES
+        </h2>
+        <div className="games-grid">
+          {gameConfigTemporary.map((game, index) => (
+            <Link
+              key={index}
+              to={game.path}
+              rel="noopener noreferrer"
+              className="game-card-link"
+            >
+              <div className="game-card">
+                <div
+                  className="game-image"
+                  style={{ backgroundImage: `url(${game.imgSrc})` }}
+                >
+                  <span className="watching">{game.watching} Watching</span>
+                </div>
+                <div className="game-info">
+                  <h3>{game.title}</h3>
+                  <p>{game.description}</p>
+                </div>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </section>
+
+      {/* Platform Stats */}
+      <section className="platform-stats">
+        <h2>📊 PLATFORM STATS</h2>
+        <div className="stats-grid">
+          <div className="stat-card">
+            <p>Total Players</p>
+            <hr />
+            <h3>133,166</h3>
+          </div>
+          <div className="stat-card">
+            <p>Total Game Volume</p>
+            <hr />
+            <h3>657,112 ETH</h3>
+          </div>
+          <div className="stat-card">
+            <p>Total Games Played</p>
+            <hr />
+            <h3>30,824,135</h3>
+          </div>
+        </div>
+      </section>
+
+      {/* Recent Wins */}
+      <section className="recent-wins">
+        <h2>🔥 RECENT WINS</h2>
+        <div className="wins-table">
+          <div className="table-header">
+            <span>Game</span>
+            <span>Player</span>
+            <span>Entry Amount</span>
+            <span>Amount Won</span>
+          </div>
+          {[...Array(8)].map((_, i) => (
+            <div className="table-row" key={i}>
+              <span>Laser Blast</span>
+              <span>Player{i + 1}</span>
+              <span>0.01 ETH</span>
+              <span className="win-amount">0.1 ETH</span>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
   );
-}
+};
 
-Home.propTypes = {};
-
-export default Home;
+export default HomePage;
---
+++ src/routes/gameConfigTemporary.js
@@ -0,0 +1,34 @@
+import LaserBlast from "../games/LaserBlast/LaserBlast";
+import laserBlastLogo from "../games/LaserBlast/assets/thumbnail.webp";
+
+const gamesConfigTemporary = [
+    {
+        "title": "Laser Blast",
+        "description": "Invaders must die!",
+        "type": "Single Player",
+        "path": "/laser-blast",
+        "imgSrc": laserBlastLogo,
+        "watching": 31,
+        "element": <LaserBlast />
+    },
+    {
+        "title": "Laser Blast",
+        "description": "Invaders must die!",
+        "type": "Single Player",
+        "path": "/laser-blast",
+        "imgSrc": laserBlastLogo,
+        "watching": 4,
+        "element": <LaserBlast />
+    },
+    {
+        "title": "Laser Blast",
+        "description": "Invaders must die!",
+        "type": "Single Player",
+        "path": "/laser-blast",
+        "imgSrc": laserBlastLogo,
+        "watching": 7,
+        "element": <LaserBlast />
+    },
+];
+
+export default gamesConfigTemporary;
