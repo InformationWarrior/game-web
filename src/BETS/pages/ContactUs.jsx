6c08e48b507413619020ef0e1b872a2c278905e9
Abhishek Sharma <abhishekjps19@gmail.com>
2025-02-10 16:34:15 +0530

    Contact Us css updated

--- src/BETS/pages/ContactUs.jsx
+++ src/BETS/pages/ContactUs.jsx
@@ -1,21 +1,21 @@
 import React from "react";
-import "../styles/ContactUs.css";
+import styles from "../../styles/BETS/ContactUs.module.css"; // Import CSS module
 
 const ContactUs = () => {
   return (
-    <div className="contact-us-container">
-      <div className="contact-us-header">
+    <div className={styles["contact-us-container"]}>
+      <div className={styles["contact-us-header"]}>
         <h1>Contact Us</h1>
         <p>
           We'd love to hear from you! Fill out the form below to get in touch.
         </p>
       </div>
-      <form className="contact-us-form">
-        <div className="form-group">
+      <form className={styles["contact-us-form"]}>
+        <div className={styles["form-group"]}>
           <label htmlFor="name">Name</label>
           <input type="text" id="name" name="name" placeholder="Your Name" />
         </div>
-        <div className="form-group">
+        <div className={styles["form-group"]}>
           <label htmlFor="email">Email</label>
           <input
             type="email"
@@ -24,7 +24,7 @@ const ContactUs = () => {
             placeholder="Your Email"
           />
         </div>
-        <div className="form-group">
+        <div className={styles["form-group"]}>
           <label htmlFor="message">Message</label>
           <textarea
             id="message"
@@ -32,7 +32,7 @@ const ContactUs = () => {
             placeholder="Your Message"
           ></textarea>
         </div>
-        <button type="submit" className="contact-us-button">
+        <button type="submit" className={styles["contact-us-button"]}>
           Send Message
         </button>
       </form>
