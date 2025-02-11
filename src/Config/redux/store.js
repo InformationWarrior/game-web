d2172f672029351151e25a915a67398d7f7baae2
Abhishek Sharma <abhishekjps19@gmail.com>
2025-02-11 23:57:21 +0530

    Code cleanup

--- src/Config/redux/store.js
+++ src/Config/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import betsReducer from "./slices/betsSlice";
-import pachinkoReducer from "./slices/pachinkoSlice";
 import wheelSpinReducer from "./slices/wheelSpinSlice"
 
 const store = configureStore({
   reducer: {
     bets: betsReducer,
-    pachinko: pachinkoReducer,
     wheelSpin: wheelSpinReducer
   },
 });
---
+++ src/WheelSpin/components/Player.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import "../styles/Player.css";
+import { FaEthereum } from "react-icons/fa";
+import styles from "../../styles/WheelSpin/PlayersList.module.css";
+import playerAvatar from "../assets/playerAvatar.png";
+function Player(props) {
+  const {
+    avatar,
+    username,
+    points,
+    percentage,
+    value,
+    isWinner,
+    borderColorClass,
+  } = props;
+
+  return (
+    <div className={styles["player-item"]}>
+      <div className={styles["player-item-content"]}>
+        <div className={styles["player-card"]}>
+          <div className={styles["player-avatar-container"]}>
+            <div className="player-avatar">
+              <img src={playerAvatar} alt={username} />
+            </div>
+          </div>
+
+          <div className={styles["player-info-container"]}>
+            <div className={styles["player-details"]}>
+              <div className={styles["player-name-container"]}>
+                <div className={styles["player-name-wrapper"]}>
+                  <p className={styles["player-name"]}>BowOctopus</p>
+                </div>
+              </div>
+
+              <div className={styles["player-points-container"]}>
+                <p className={styles["player-points"]}>500 Pts</p>
+              </div>
+            </div>
+
+            <div className={styles["player-stats-container"]}>
+              <div className={styles["player-chance-container"]}>
+                <p className={styles["player-chance"]}>100%</p>
+              </div>
+
+              <div className={styles["player-currency-container"]}>
+                <p className={styles["player-currency"]}>0.01</p>
+                {/* <FaEthereum className={styles["player-currency-icon"]} /> */}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Player;
