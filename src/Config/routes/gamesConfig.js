493556598a15d2eb39276525d2624ab13e22a10a
Abhishek Sharma <abhishekjps19@gmail.com>
2025-02-14 14:38:19 +0530

    Code push

--- src/Config/routes/gamesConfig.js
+++ src/Config/routes/gamesConfig.js
@@ -1,18 +1,7 @@
-import PocketPachinko from '../../Games/Pachinko/PocketPachinko'
-import pocketPachinkoLogo from '../../Games/Pachinko/assets/thumbnail.webp'
-
 import WheelSpin from '../../Games/WheelSpin/WheelSpin'
 import wheelSpinLogo from '../../Games/WheelSpin/assets/SpinWheelBanner.webp'
 
 const gamesConfig = [
-  // {
-  //     "title": "Pocket Pachinko",
-  //     "description": "Invaders must die!",
-  //     "type": "single",
-  //     "path": "/pocket-pachinko",
-  //     "imgSrc": pocketPachinkoLogo,
-  //     "element": <PocketPachinko />
-  // },
   {
     id: '67ab8f01d7f39d41a761fc67', // Temporary ID until backend provides actual ID
     title: 'Wheel Spin',
